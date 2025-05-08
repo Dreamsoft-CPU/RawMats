b92d2617c1ab3801ee9b6b4565602f3f696bde46
chdean-09 <chadandrada20@gmail.com>
2025-05-08 14:57:39 +0800

    fix: logout error message bug

--- src/utils/server/logout.ts
+++ src/utils/server/logout.ts
@@ -2,9 +2,11 @@
 
 import { revalidatePath } from "next/cache";
 import { createClient } from "../supabase/server";
+import { redirect } from "next/navigation";
 
 export const logout = async () => {
   const supabase = await createClient();
   await supabase.auth.signOut();
   revalidatePath("/");
+  redirect("/");
 };
